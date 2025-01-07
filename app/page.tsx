b0836da316e979ed3677e79419af20c4c8260f24
Gustavo pontes <gustavo.4dev@gmail.com>
2025-01-07 14:08:57 -0300

    Update page metadata title and description

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { type ReactNode } from "react";
 import { HomeTemplate } from "@/templates/home";
 
 export const metadata: Metadata = {
-  title: "Fiber web - Creative Webseiten",
-  description: "Creative Webseiten Effektives Marketing Smarte KI Lösungen",
+  title: "Büro im Flow",
+  description: "Büro im Flow",
 };
 
 const Home = (): ReactNode => {
