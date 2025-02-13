ab161f71da025cc5be91b69047e4144687aeaa02
Gustavo pontes <gustavo.4dev@gmail.com>
2025-02-13 11:53:04 -0300

    Add sitemap generation for dynamic routing and SEO optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,43 @@
+import type { MetadataRoute } from "next";
+import listPosts from "@/api/list-posts";
+
+export const revalidate = 60;
+export const dynamic = "force-static";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await listPosts();
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/uber-uns/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/dienstleistungen/`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/blog/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    ...posts.map((post) => ({
+      url: `${process.env.NEXT_PUBLIC_URL}/blog/${post.json.link}/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly" as const,
+      priority: 0.8,
+    })),
+  ];
+}
